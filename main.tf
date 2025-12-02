15a2c23f3f2ce6d36a7b2eab8fed7c53fc0acb1a
kishore babu <shkishore8@gmail.com>
2025-12-02 06:28:27 +0530

    terraform

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "web" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "test1"
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z04751093FJLNWEV5LUJS"
+  name    = "test"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.web.private_ip]
+}
